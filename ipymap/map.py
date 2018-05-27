59fd1bda4cc4b0d0807555f8b25f82a5c9b0619b
y² <yy@yuyuan.org>
2018-05-26 18:27:54 -0700

    pep8

--- ipymap/map.py
+++ ipymap/map.py
@@ -1,6 +1,8 @@
 from string import Template
+from IPython.display import display
 
 import urllib.request
+import urllib.error
 import json
 import csv
 
@@ -11,49 +13,46 @@ import ipywidgets as widgets
 class USZIPCodeRepository:
     CACHE = {}
 
-    def __init__(self, data_url_prefix = 'https://raw.githubusercontent.com/yyu/GeoJSON-US/master'):
+    def __init__(self, data_url_prefix='https://raw.githubusercontent.com/yyu/GeoJSON-US/master'):
         self.data_url_prefix = data_url_prefix
         self.geojson_url_prefix = f'{data_url_prefix}/perZIPgeojson'
 
-        self.refresh_zipcode_latlons(f'{data_url_prefix}/ZIPCodesGazetteer.tsv')
-        self.refresh_available_zipcodes(f'{data_url_prefix}/perZIPgeojson/all_zipcodes.txt')
-
-
-    def refresh_zipcode_latlons(self, url):
-        lines = [ line.decode('UTF8').strip() for line in urllib.request.urlopen(url).readlines() ]
-        tsv = csv.DictReader(lines, delimiter='\t')
-        self.gazetteer = dict((d['GEOID'], {'lat': float(d['INTPTLAT']), 'lon': float(d['INTPTLONG'])}) for d in tsv)
-
-
-    def refresh_available_zipcodes(self, url):
-        lines = [ zipcode.decode('UTF8').strip() for zipcode in urllib.request.urlopen(url).readlines() ]
-        self.zipcode_list = lines[1:] # ignore the first line
+        self.gazetteer = self.get_zipcode_latlons(f'{data_url_prefix}/ZIPCodesGazetteer.tsv')
+        self.zipcode_list = self.get_available_zipcodes(f'{data_url_prefix}/perZIPgeojson/all_zipcodes.txt')
         self.zipcode_set = set(self.zipcode_list)
 
+    @staticmethod
+    def get_zipcode_latlons(url):
+        lines = [line.decode('UTF8').strip() for line in urllib.request.urlopen(url).readlines()]
+        tsv = csv.DictReader(lines, delimiter='\t')
+        return dict((d['GEOID'], {'lat': float(d['INTPTLAT']), 'lon': float(d['INTPTLONG'])}) for d in tsv)
+
+    @staticmethod
+    def get_available_zipcodes(url):
+        lines = [zipcode.decode('UTF8').strip() for zipcode in urllib.request.urlopen(url).readlines()]
+        return lines[1:]  # ignore the first line
 
     def make_url(self, zipcode):
         return f'{self.data_url_prefix}/perZIPgeojson/{zipcode[0]}/{zipcode[1]}/{zipcode[2]}/{zipcode}.json'
 
-
     def fetch_zipcode(self, zipcode):
-        '''returns a (dict, err) tuple where err could be a string for error message or None'''
+        """returns a (dict, err) tuple where err could be a string for error message or None"""
 
         url = self.make_url(zipcode)
 
         if url in USZIPCodeRepository.CACHE:
-            return (USZIPCodeRepository.CACHE[url], None)
+            return USZIPCodeRepository.CACHE[url], None
 
         try:
             s = urllib.request.urlopen(url).read()
         except urllib.error.URLError as e:
-            return (None, 'failed to get ' + url, ':', e.reason)
+            return None, 'failed to get ' + url + ':' + e.reason
 
         j = json.loads(s)
 
         USZIPCodeRepository.CACHE[url] = j
 
-        return (j, None)
-
+        return j, None
 
     def fetch_zipcodes(self, *zipcodes):
         d = {"type": "FeatureCollection", "features": []}
@@ -77,7 +76,7 @@ class USMap:
         self.zoom = 8
         self.height = '500px'
         self.progress_bar_width = '500px'
-        self.area_style = {'color':'#0000ff', 'weight': .5, 'fillColor':'#000077', 'fillOpacity':0.2}
+        self.area_style = {'color': '#0000ff', 'weight': .5, 'fillColor': '#000077', 'fillOpacity': 0.2}
 
         self.progress_bar = widgets.IntProgress(bar_style='info', layout=widgets.Layout(width=self.progress_bar_width))
         self.label = widgets.Label()
@@ -94,17 +93,14 @@ class USMap:
 
         self.map = None
 
-
     def enable_heatmap(self):
         self.map.add_layer(self.heatmap)
-
 
     def disable_heatmap(self):
         self.map.remove_layer(self.heatmap)
 
-
     def handle_interaction(self, **kwargs):
-        '''mouse interaction handling'''
+        """mouse interaction handling"""
         if kwargs.get('type') == 'mousemove':
             self.label.value = str(kwargs.get('coordinates'))
 
@@ -114,11 +110,9 @@ class USMap:
             print(err)
         return d
 
-
     def add_point(self, lat, lng, name='', popup=None):
         feature = {"type": "Feature", "properties": {}, "geometry": {"type": "Point", "coordinates": [lng, lat]}}
         self.add_geojson(feature, name, popup)
-
 
     def add_geojson(self, geojson, name='', popup=None):
         g = leaflet.GeoJSON(data=geojson, hover_style={'fillColor': '#00aaff'}, name=name)
@@ -128,12 +122,10 @@ class USMap:
 
         self.map += g
 
-
     def add_geojsons(self, geojsons, name=''):
         d = {"type": "FeatureCollection", "features": list(geojsons)}
 
         self.add_geojson(d, name)
-
 
     def add_zipcode(self, zipcode):
         d = self.fetch_zipcode(zipcode)
@@ -153,7 +145,6 @@ class USMap:
 
         self.add_geojson(d, name=zipcode, popup=popup)
 
-
     def add_zipcodes(self, zipcodes):
         display(self.info_box)
 
@@ -171,7 +162,6 @@ class USMap:
         self.progress_label.value = '%d/%d loaded' % (len(available_zipcodes), len(zipcodes))
 
         return available_zipcodes
-
 
     def display(self):
         if self.map is None:
